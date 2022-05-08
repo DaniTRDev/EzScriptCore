0c1dc59eea1c989a7c70445912393d79d53ee162
danitrchetadas <danitrchetadas@gmail.com>
2022-05-08 19:45:42 +0200

    fix(EzSignature): Fixed the signature generator & scanner. Now it gives proper results.

--- EzScriptCore/EzSignature/EzSignature.cpp
+++ EzScriptCore/EzSignature/EzSignature.cpp
@@ -2,8 +2,9 @@
 
 namespace Ez
 {
-	EzSignatureStatus EzSignature::SetSignatureContext(std::uint8_t* OpCodes, std::size_t OpCodeNum,
-		EzInstruction** Instructions, std::size_t NumInstrs, rage::RageResourceVersion Version)
+	EzSignatureStatus EzSignature::SetSignatureContext(
+		std::uint8_t* OpCodes, std::size_t OpCodeNum, std::shared_ptr<EzInstruction>* Instructions, 
+		std::size_t NumInstrs, rage::RageResourceVersion Version)
 	{
 		if (m_Instructions)
 			return EzSignatureStatus::ContextAlreadySet;
@@ -25,64 +26,158 @@ namespace Ez
 		m_Instructions = Instructions;
 		m_NumInstructions = NumInstrs;
 		m_Version = Version;
+		m_SignatureAddress = 0;
+		m_FoundAddress = 0;
 
 		return EzSignatureStatus::NoError;
 	}
-	EzSignatureStatus EzSignature::RetrieveFromAddress(const std::uintptr_t& Address)
+
+	EzSignatureStatus EzSignature::CreateAtAddress(std::uintptr_t Address)
 	{
 		if (!m_Instructions)
 			return EzSignatureStatus::ContextNotSet;
 
+		m_SignatureAddress = Address;
+		bool AreWeOnCorrectAddr = false;
 		for (auto i = 0; i < m_NumInstructions; i++)
 		{
 			auto Instruction = m_Instructions[i];
 			auto InstructionBytes = &m_OpCodes[Instruction->GetStartAddr()];
+			EzSignatureInstr SigInstr{ Instruction->GetStartAddr(), Instruction->GetInstructionSize(), false};
 
-			if (Instruction->GetStartAddr() != Address)
-				continue;
+			if (!AreWeOnCorrectAddr)
+			{
+				if ((Instruction->GetStartAddr() != Address))
+					continue;
 
-			auto SignatureBytes = Instruction->GetInstructionSize(); 
-			/*the entire instruction is going to be added to the sig if the if above doesnt get 
-			executed*/
+				AreWeOnCorrectAddr = true;
+			}
 
 			if (Instruction->IsCall() || Instruction->IsJump()
-				|| Instruction->IsVarLoad() || Instruction->IsVarPush() || Instruction->IsVarStore())
+				|| Instruction->IsVarLoad() || Instruction->IsVarPush() || Instruction->IsVarStore()
+				|| Instruction->IsNativeCall())
 			{
-				//the bytes of the operand(s) of these instructions will surely change between updates
-				m_Signature += std::to_string(Instruction->GetInstructionId());
-				SignatureBytes = 1;
+				SigInstr.m_HasWildCards = true;
 			}
-			//the rest of cases won't change betweem updates
-			for (auto i = 0; i < Instruction->GetInstructionSize(); i++)
-			{
-				if (SignatureBytes != Instruction->GetInstructionSize())
-				{
-					/*we are dealing with opcodes that can change between updates, mark them as wildcards*/
-					if (i == 0) /*we already added the first opcode*/
-						continue;
+			
+			AddInstruction(SigInstr);
 
-					if (i > (SignatureBytes - 1))
-						m_Signature += ' ??';
-				}
-				else
-					m_Signature += std::to_string(InstructionBytes[i]);
+			auto IsSigValid = ScanFromNewSig(m_Signature.str()) == EzSignatureStatus::NoError;
+
+			if (IsSigValid) /*sig had a match, lets check if it's the one we want*/
+			{
+				if (m_FoundAddress == m_SignatureAddress)
+					return EzSignatureStatus::NoError;
 			}
-			/*now that we added an instruction to the signature, scan for it in the script so we know
-			when we have a good signature*/
 		}
 
 		return EzSignatureStatus::CouldNotCreateSignatureForAddress;
 	}
-	EzSignatureStatus EzSignature::ScanAtAddress(const std::uintptr_t& Address)
+	EzSignatureStatus EzSignature::ScanFromNewSig(std::string Signature)
+	{
+		m_Signature.clear();
+		m_Signature.str("");
+		m_SignatureBytes.clear();
+		m_FoundAddress = 0;
+		m_Signature << Signature;
+
+		Signature2Bytes();
+
+		return ScanAtAddress(0); /*0 is the starting addr of m_OpCodes array*/
+	}
+
+	EzSignatureStatus EzSignature::ScanAtAddress(std::uintptr_t Address)
 	{
 		if (!m_Instructions)
 			return EzSignatureStatus::ContextNotSet;
 
-		for (auto i = 0; i < m_NumInstructions; i++)
+		for (auto OpCodeId = 0; OpCodeId < m_NumOpCodes; OpCodeId++)
 		{
+			auto Bytes = (std::uint8_t*)&m_OpCodes[OpCodeId];
+			for (auto i = 0; i < m_SignatureBytes.size(); i++)
+			{
+				if (m_SignatureBytes[i] == 0) /*wildcard*/
+				{
+					goto SigPosibleMatchFound;
+					continue;
+				}
 
+				else if (m_SignatureBytes[i] != Bytes[i])
+					break;
+			
+
+				SigPosibleMatchFound:
+				if (i == (m_SignatureBytes.size() - 1))
+				{
+					m_FoundAddress = OpCodeId;
+					return EzSignatureStatus::NoError;
+				}
+			}
+		}
+
+		return EzSignatureStatus::CouldNotRetrieveSignatureForAddress;
+	}
+
+	std::string EzSignature::GetSignature()
+	{
+		return m_Signature.str();
+	}
+	EzSignatureStatus EzSignature::Signature2Bytes()
+	{
+		auto Str = m_Signature.str();
+
+		for (auto i = 0; i < Str.length(); i++)
+		{
+			if (Str[i] == ' ') /*skip spaces*/
+				continue;
+
+			if (Str[i] == '?')
+			{
+				if (Str[i + std::uint64_t(1)] == '?')
+					i++;
+
+				m_SignatureBytes.push_back(0); /*we don't take care of wildcards*/
+
+				continue;
+			}
+
+			char Bytes[] = { Str[i], Str[i + std::uint64_t(1)] }; /*store the 2bytes in a single char*/
+
+			auto ConvertedBytes = strtoull(Bytes, nullptr, 16);
+
+			m_SignatureBytes.push_back(std::uint8_t(ConvertedBytes));
+
+			i++;
 		}
 
 		return EzSignatureStatus::NoError;
+	}
+
+	void EzSignature::AddWhiteSpace()
+	{
+		m_Signature << " ";
+	}
+	void EzSignature::AddWildCard()
+	{
+		AddWhiteSpace();
+		m_Signature << "?";
+	}
+
+	void EzSignature::AddByte(std::uint8_t Byte)
+	{
+		AddWhiteSpace();
+		m_Signature << std::setw(2) << std::setfill('0') << std::hex << std::uint16_t(Byte);
+	}
+	void EzSignature::AddInstruction(const EzSignatureInstr& Instr)
+	{
+		for (auto i = Instr.m_Address; i < Instr.m_Address + Instr.m_Size; i++)
+		{
+			if (Instr.m_HasWildCards && (i != Instr.m_Address))
+				AddWildCard();
+
+			else
+				AddByte(m_OpCodes[i]);
+
+		}
 	}
 }
--- EzScriptCore/EzSignature/EzSignature.h
+++ EzScriptCore/EzSignature/EzSignature.h
@@ -11,7 +11,16 @@ namespace Ez
 		InvalidOpCodes,
 		InvalidOpCodeCount,
 		CouldNotCreateSignatureForAddress,
+		CouldNotRetrieveSignatureForAddress,
 		NoError
+	};
+
+	struct EzSignatureInstr
+	{
+		std::uintptr_t m_Address;
+		std::size_t m_Size; /*determines how many opcodes the instruction has*/
+		bool m_HasWildCards; /*if this is true then the signature will only contain the first
+							opcode which is the instruction id the rest will be filled with '?'*/
 	};
 
 	class EzSignature
@@ -19,20 +28,38 @@ namespace Ez
 	public:
 
 		EzSignatureStatus SetSignatureContext(std::uint8_t* OpCodes, std::size_t OpCodeNum,
-			EzInstruction** Instructions, std::size_t NumInstrs, rage::RageResourceVersion Version);
+			std::shared_ptr<EzInstruction>* Instructions, std::size_t NumInstrs, rage::RageResourceVersion Version);
 
-		EzSignatureStatus RetrieveFromAddress(const std::uintptr_t& Address);
-		EzSignatureStatus ScanAtAddress(const std::uintptr_t& Address);
+		EzSignatureStatus CreateAtAddress(std::uintptr_t Address);
+		EzSignatureStatus ScanFromNewSig(std::string Signature);
+
+		EzSignatureStatus ScanAtAddress(std::uintptr_t Address);
+
+		std::string GetSignature();
+		EzSignatureStatus Signature2Bytes();
 
 	private:
-		EzInstruction** m_Instructions;
+
+		void AddWhiteSpace();
+		void AddWildCard();
+
+		void AddByte(std::uint8_t Byte);
+		void AddInstruction(const EzSignatureInstr& Instr);
+
+	private:
+		std::shared_ptr<EzInstruction>* m_Instructions;
 		std::size_t m_NumInstructions;
 
 		std::uint8_t* m_OpCodes;
 		std::size_t m_NumOpCodes;
 		rage::RageResourceVersion m_Version;
 
-		std::string m_Signature;
+		std::uintptr_t m_SignatureAddress; /*the address where it was generated*/
+		std::uintptr_t m_FoundAddress; /*the last address where it was found*/
+	
+	
+		std::ostringstream m_Signature;
+		std::vector<std::uint8_t> m_SignatureBytes;
 	};
 }
 
