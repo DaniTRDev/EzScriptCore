fa9b106a6bde2b4ba3d0a2498a0e88f5af7a42f3
danitrchetadas <danitrchetadas@gmail.com>
2022-05-08 19:43:47 +0200

    feat(GlobalUpdater); Added a few scrips to the directory to start working on the proper updater.

--- EzGlobalUpdater/EzGlobalUpdater.cpp
+++ EzGlobalUpdater/EzGlobalUpdater.cpp
@@ -8,96 +8,101 @@
 
 //#include "EzGlobalUpdater.h"
 
+#include <include/VariadicTable.h>
 #include "pch.h"
+
+using ScriptTable_t = VariadicTable<std::string, std::size_t, std::size_t, std::size_t,
+    std::size_t, std::size_t, std::size_t, std::size_t, std::size_t>;
+
+ScriptTable_t ScriptTable({ "Name", "Locals", "Script Params", "String", "Natives",
+        "Blocks", "Total Code Lenght", "Functions", "Instructions" });
+
+void AddNewScript2Table(Ez::EzDecompiler* Decompiler)
+{
+    ScriptTable.addRow(Decompiler->GetScriptName(), Decompiler->GetLocals().size(), 
+        Decompiler->GetScriptParams().size(), Decompiler->GetStrings().size(), 
+        Decompiler->GetNatives().size(), Decompiler->GetBlocksNumber(), Decompiler->GetCodeSize(),
+        Decompiler->GetFunctions().size(), Decompiler->GetMappedInstructions());
+}
+
+void TestSignatureCreation(Ez::EzDecompiler& Decompiler)
+{
+    auto TestAddr = 0xad;
+    auto Func = Decompiler.GetFuncFromOffset(TestAddr);
+    auto& Instrs = const_cast<std::vector<std::shared_ptr<Ez::EzInstruction>>&>
+        (Func->GetInstructions());
+
+    auto Signature = Ez::EzSignature();
+    Signature.SetSignatureContext(Decompiler.GetCode(), Decompiler.GetCodeSize(),
+        Instrs.data(), Instrs.size(), Ez::rage::RageResourceVersion::RRV_MinusThan10);
+
+    auto Res = Signature.CreateAtAddress(TestAddr);
+    auto SigStr = Signature.GetSignature();
+
+    auto Found = Signature.ScanFromNewSig(SigStr);
+
+    std::cout << "Created signature at: 0x" << std::hex << TestAddr
+        << "\t: " << Signature.GetSignature() << std::endl;
+}
+
+Ez::EzDecompiler * OpenScript(std::filesystem::path InPath, std::filesystem::path OutPath)
+{
+    std::ifstream InStream(InPath.string(), std::ios::in | std::ios::binary);
+    std::ofstream OutStream(OutPath.string(), std::ios::out | std::ios::binary | std::ios::trunc);
+
+    if (!InStream.is_open())
+    {
+        std::cout << "Could not open input file, check path: " << InPath.string() << std::endl;
+        return nullptr;
+    }
+    else if (!OutStream.is_open())
+    {
+        std::cout << "Could not open output file, check path: " << OutPath.string() << std::endl;
+        return nullptr;
+    }
+
+    auto Script = new Ez::EzSrcProgram();
+
+    if (auto Result = Script->ParseHeader(InStream, ""); Result != Ez::EzScriptStatus::NoError)
+    {
+        std::cout << "Error " << static_cast<std::uint32_t>(Result) << std::endl;
+        return nullptr;
+    }
+
+    auto Decompiler = new Ez::EzDecompiler(Script);
+
+    if (auto Result = Decompiler->MapScript(); Result != Ez::EzDecompilerStatus::NoError)
+        std::cout << "Error " << static_cast<std::uint32_t>(Result) << std::endl;
+
+    if (auto Result = Decompiler->Disassemble(); Result != Ez::EzDecompilerStatus::NoError)
+        std::cout << "Error " << static_cast<std::uint32_t>(Result) << std::endl;
+
+    auto& Assembly = Decompiler->GetAssembly();
+    OutStream << Assembly.str() << std::endl;
+
+    AddNewScript2Table(Decompiler);
+
+    Decompiler->FreeDisassemblyBuffer();
+    InStream.close();
+    OutStream.close();
+
+    return Decompiler;
+}
 
 void UpdateGlobals()
 {
-    //Ez::EzScriptFile ScrFile("C:\\Users\\danit\\source\\repos\\EzGlobalUpdater\\x64\\Release\\am_gang_call.ysc.full");
+    auto CurrentPath = std::filesystem::current_path();
 
-    std::ifstream Stream;
-    Stream.open("C:\\Users\\danit\\source\\repos\\EzGlobalUpdater\\x64\\Release\\am_gang_call.ysc.full", std::fstream::in | std::fstream::binary);
-   
-    std::ofstream Output;
-    Output.open("C:\\Users\\danit\\source\\repos\\EzGlobalUpdater\\x64\\Release\\am_gang_call.assembly.txt", std::fstream::out | std::fstream::binary | std::fstream::trunc);
+    auto Script1 =  OpenScript(CurrentPath / "am_gang_call.ysc.full", 
+        CurrentPath / "am_gang_call_disam.txt");
 
-    Ez::EzSrcProgram Script;
-    if (auto Result = Script.ParseHeader(Stream, "am_gang_call"); Result != Ez::EzScriptStatus::NoError)
-        std::cout << "Error " << static_cast<std::uint32_t>(Result) << std::endl;
+    auto Script2 = OpenScript(CurrentPath / "gpb_tonya.ysc.full",
+        CurrentPath / "gpb_tonya.txt");
 
-    Ez::EzDecompiler Decompiler(&Script);
+    auto Script3 = OpenScript(CurrentPath / "shop_controller.ysc.full",
+        CurrentPath / "shop_controller.txt");
 
-    if (auto Result = Decompiler.MapLocals(); Result != Ez::EzDecompilerStatus::NoError)
-        std::cout << "Error " << static_cast<std::uint32_t>(Result) << std::endl;
-
-    printf("Mapped %zu locals\n", Decompiler.GetLocals().size());
-    printf("Mapped %zu script params\n", Decompiler.GetScriptParams().size());
-
-    if (auto Result = Decompiler.MapStrings(); Result != Ez::EzDecompilerStatus::NoError)
-        std::cout << "Error " << static_cast<std::uint32_t>(Result) << std::endl;
-
-    printf("Mapped %zu strings\n", Decompiler.GetStrings().size());
-
-    if (auto Result = Decompiler.MapNatives(); Result != Ez::EzDecompilerStatus::NoError)
-        std::cout << "Error " << static_cast<std::uint32_t>(Result) << std::endl;
-
-    printf("Mapped %zu natives\n", Decompiler.GetNatives().size());
-
-    if (auto Result = Decompiler.MapCodeBlocks(); Result != Ez::EzDecompilerStatus::NoError)
-        std::cout << "Error " << static_cast<std::uint32_t>(Result) << std::endl;
-
-    printf("Mapped %zu blocks -> Merged in a single block of size 0x%zx\n", 
-            Decompiler.GetBlocksNumber(), Decompiler.GetBlockSize());
-
-    if (auto Result = Decompiler.MapFunctions(); Result != Ez::EzDecompilerStatus::NoError)
-        std::cout << "Error " << static_cast<std::uint32_t>(Result) << std::endl;
-
-    printf("Mapped %zu functions\n", Decompiler.GetFunctions().size());
-
-    if (auto Result = Decompiler.MapInstructionsFromFuncs(); Result != Ez::EzDecompilerStatus::NoError)
-        std::cout << "Error " << static_cast<std::uint32_t>(Result) << std::endl;
-
-    printf("Predecompiled functions, generated %zu instructions\n", Decompiler.GetMappedInstructions());
-
-    if (auto Result = Decompiler.Disassemble(); Result != Ez::EzDecompilerStatus::NoError)
-        std::cout << "Error " << static_cast<std::uint32_t>(Result) << std::endl;
-
-    Output << Decompiler.GetAssembly().str() << std::endl;
-
-    /*if (!ScrFile.ParseHeader("freemode"))
-    {
-        std::cout << "Error while parsing the header!" << std::endl;
-        return;
-    }
-
-    if (!ScrFile.MapLocals())
-    {
-        std::cout << "Error while mapping locals!" << std::endl;
-        return;
-    }
-
-    if (!ScrFile.MapCodeBlocks())
-    {
-        std::cout << "Error while mapping code blocks!" << std::endl;
-        return;
-    }
-
-    if (!ScrFile.MapFunctions())
-    {
-        std::cout << "Error while mapping functions from blocks!" << std::endl;
-        return;
-    }
-
-    if (!ScrFile.MapFuncsInstructions())
-    {
-        std::cout << "Error while mapping instructions from functions!" << std::endl;
-        return;
-    }
-
-    if (!ScrFile.DecompileFuncsInstructions())
-    {
-        std::cout << "Error while decompiling instructions from functions!" << std::endl;
-        return;
-    }*/
+    ScriptTable.print(std::cout);
 }
 
 int main()
